610dfdc20a3ad053abc676335430718e72c13515
tomasbula <tomas.bula@post.cz>
2025-10-12 18:54:20 +0200

    Update main.py
    Úprava kódu na základě připomínek

--- main.py
+++ main.py
@@ -1,73 +1,79 @@
 import random
 import time
 
-def separator_1():      # Funkce pro oddělovací čáru
-    """Oddělovací čára"""
+number_length = 4  # Length of the number
+
+def separator_1():      # Function for single separator line
+    """Single separator line"""
     print("-" * 50)
 
-def separator_2():      # Funkce pro dvojitou oddělovací čáru
-    """Dvojitá oddělovací čára"""
+def separator_2():      # Function for double separator line
+    """Double separator line"""
     print("=" * 50)
 
-def secret_number_generator():  # Funkce pro generování náhodného 4-místného čísla
+def secret_number_generator():  # Function to generate a random number with unique digits
     """
-    Generování náhodného 4-místného čísla s unikátními číslicemi
+    Generating a random number with unique digits
     
     Returns:
-        int: Náhodně vygenerované 4-místné číslo
+        int: Randomly generated number with unique digits
     """
+    digits = list("0123456789")
     while True:
-        generated_number = random.randint(1000, 9999)
-        digits = str(generated_number)
-        if len(set(digits)) == 4:  # kontrola unikátnosti číslic
-            break
-    return generated_number
+        number = random.sample(digits, number_length)
+        if number[0] != '0':
+            return int("".join(number))
 
-def get_user_guess():   # Funkce pro číslo zadané uživatelem
+def get_user_guess():   # Function to get user input
     """
-    Získání a validace čísla zadaného uživatelem
+    Getting and validating user input for a number with unique digits
     
     Returns:
-        int: Validované 4-místné číslo zadané uživatelem
+        int: Validated number input by the user
     """
     while True:
         user_input = input(">>> ")
-        # Kontrola, zda zadané číslo nezačíná nulou
+        # Empty input check
+        if not user_input:
+            print("Input cannot be empty.")
+            separator_1()
+            continue
+        # Check if the number not starts with 0
         if user_input[0] == '0':
             print("Number must not start with 0.")
             separator_1()
             continue
-        # Kontrola, zda jsou zadány pouze číslice
+        # Check if input contains only digits
         if not user_input.isdigit():                    
             print("Please enter digits only.")
             separator_1()
             continue
-        # Kontrola délky zadaného čísla (pouze 4 číslice)
-        if len(user_input) != 4:
-            print("Please enter a 4-digit number.")
+        # Digits length check
+        if len(user_input) != number_length:
+            print(f"Please enter a {number_length}-digit number.")
             separator_1()
             continue
-        # Kontrola unikátnosti číslic
-        if len(set(user_input)) != 4:
+        # Digits uniqueness check
+        if len(set(user_input)) != number_length:
             print("Digits must be unique.")
             separator_1()
             continue
         return int(user_input)
 
-def number_to_list(number): # Funkce pro převod čísla na list číslic
-    """Převod 4-místných čísel na listy číslic"""
+def number_to_list(number): # Function to convert a number to a list of its digits
+    """Convert a number to a list of its digits"""
     return [int(digit) for digit in str(number)]
 
-def game_bulls_and_cows():      # Funkce pro spuštění hry
+def game_bulls_and_cows():      # Function for the main game starting and logic
     """
-    Hlavní funkce pro hru Bulls & Cows
+    Main function for the Bulls & Cows game.
     
-    Hráč hádá náhodně vygenerované 4-místné číslo s unikátními číslicemi.
-    Po každém pokusu hráče se vypíše počet "bulls" a "cows"
-    Hra pokračuje, dokud hráč neuhodne správné číslo.
-    Po uhodnutí čísla se vypíše počet pokusů a celkový čas hry.
+    The player guesses a randomly generated 4-digit number with unique digits.
+    After each attempt by the player, the number of "bulls" and "cows" is displayed
+    The game continues until the player guesses the correct number.
+    After guessing the number, the number of attempts and the total time of the game are displayed.
     """
-    # Úvod a představení hry Bulls & Cows
+    # Introduction of the game Bulls & Cows
     separator_2()
     print("Hi there!")
     separator_1()
@@ -75,76 +81,68 @@ def game_bulls_and_cows():      # Funkce pro spuštění hry
     separator_1()
     print("Enter a number:")
     separator_1()
-    # Hlavní program hry
-    secret_number = secret_number_generator()   # Generování tajného čísla
-    user_number = 0         # Výchozí hodnota pro uživatelské číslo
-    attempts = 0            # Počet pokusů uživatele - výchozí hodnota
+    # Main game logic
+    secret_number = secret_number_generator()   # Generating the secret number
+    user_number = 0         # Default user number
+    attempts = 0            # Attempts counter - default value
     
-    start = time.time()  # Start měření času   
+    start = time.time()  # Time measurement start   
     while True:
-        attempts += 1   # Počítadlo pokusů
-        user_number = get_user_guess()  # Získání čísla od uživatele
-        secret_number_to_list = number_to_list(secret_number)   # Převod tajného čísla na list
-        user_number_to_list = number_to_list(user_number)       # Převod uživatelského čísla na listc
-        total_bulls = 0     # Celkový počet bulls - výchozí hodnota
-        total_cows = 0      # Celkový počet cows - výchozí hodnota
-        # Pomocné seznamy pro pozice, které už byly použity
-        bulls_indices = []
-        cows_indices_secret = []
-        cows_indices_user = []
+        attempts += 1   # Attempts counter increment
+        user_number = get_user_guess()  # Getting user input
+        secret_number_to_list = number_to_list(secret_number)   # Conversion of the secret number to a list
+        user_number_to_list = number_to_list(user_number)       # Conversion of the user number to a list
 
-        for i in range(4):      # Hledání bulls a jejich pozic
-            if user_number_to_list[i] == secret_number_to_list[i]:
-                total_bulls += 1
-                bulls_indices.append(i)
+        # To find indices where bulls are (same number in the same place)
+        bull_indices = [i for i, (u, s) in enumerate(zip(user_number_to_list, secret_number_to_list)) if u == s]
+        total_bulls = len(bull_indices)
 
-        for i in range(4):      # Hledání cows s ohledem na již nalezené bulls
-            if i in bulls_indices:
-                continue  # Přeskoč bulls
+        # To get remaining digits for cows (those that are not bulls)
+        user_remaining = [u for i, u in enumerate(user_number_to_list) if i not in bull_indices]
+        secret_remaining = [s for i, s in enumerate(secret_number_to_list) if i not in bull_indices]
 
-            for j in range(4):
-                if j in bulls_indices or j in cows_indices_secret:
-                    continue  # Nepoužívej pozice označené jako bulls nebo použité pro jinou cow
-                if user_number_to_list[i] == secret_number_to_list[j]:
-                    total_cows += 1
-                    cows_indices_secret.append(j)
-                    cows_indices_user.append(i)
-                    break
-        
-        if total_bulls == 4:    # Výpis při správném uhodnutí čísla
+        # To count cows (correct number, wrong place)
+        total_cows = 0
+        for u in user_remaining:
+            if u in secret_remaining:
+                total_cows += 1
+                secret_remaining.remove(u)  # So as not to be counted more than once
+
+        if total_bulls == 4:    # Statement when guessing the number correctly
             print(f"Correct, you've guessed the right number\nin {attempts} guesses!")
             separator_1()
             print("That's amazing!")
             separator_1()
             break         
-        else:   # Výpis bulls & cows s ohledem na jednotné/množné číslo
+        else:   # Bulls & cows listing with respect to singular/plural
             bull_word = "bull" if total_bulls == 1 else "bulls"
             cow_word = "cow" if total_cows == 1 else "cows"
             print(f"{total_bulls} {bull_word}, {total_cows} {cow_word}")
             separator_1()   
-    end = time.time()  # Konec měření času
+    end = time.time()  # Measurement end time
     
-    # Celkový čas hry
+    # Total game time
     total_time = end - start
     minutes = int(total_time // 60)
     seconds = int(total_time % 60)   
     print(f"Total time: {minutes} min. and {seconds} sec.")
     separator_1()
 
-def repeat_the_game():      # Funkce pro opakování hry
+def repeat_the_game():      # Function to repeat the game
     """
-    Funkce pro opakování hry Bulls & Cows
+    Function to repeat the Bulls & Cows game.
     
-    Po skončení jedné hry se uživatele zeptá, zda chce hrát znovu:
-    "yes" => hra se restartuje / "no" => program se ukončí s poděkováním.
+    After finishing one game, the user is asked if he wants to play again:
+    "yes" => the game restarts / "no" => the program exits with thanks.
     """
     while True:
         game_bulls_and_cows()
-        # Dotaz na opakování hry s ošetřením na velká/malá písmena a odstranění mezer
+        # Game repetition question with input validation
         again = input("Do you want to play again? (yes/no): ").strip().lower()
         separator_1()
         if again != "yes":
             print("Thanks for playing! Goodbye.")
             break
 
-repeat_the_game()
+if __name__ == "__main__":
+    repeat_the_game()
